656363b592503b9d3ede3764e466426449c65954
kurtasov <kurtasov@users.noreply.github.com>
2023-03-02 08:23:27 +0300

    connected expense_presenter.py to expenses

--- bookkeeper/main.py
+++ bookkeeper/main.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QApplication
 from bookkeeper.view.expense_view import MainWindow
 from bookkeeper.presenter.expense_presenter import ExpensePresenter
 from bookkeeper.models.category import Category
+from bookkeeper.models.expense import Expense
 from bookkeeper.repository.sqlite_repository import SQLiteRepository
 import sys
 
@@ -9,9 +10,13 @@ DB_NAME = 'test.db'
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
     view = MainWindow()
     model = None  # TODO: здесь должна быть модель
-    cat_repo = SQLiteRepository[Category](DB_NAME, Category)  # TODO: здесь должны быть расходы
-    window = ExpensePresenter(model, view, cat_repo)  # TODO: передать три репозитория
+
+    cat_repo = SQLiteRepository[Category](DB_NAME, Category)
+    exp_repo = SQLiteRepository[Expense](DB_NAME, Expense)
+
+    window = ExpensePresenter(model, view, cat_repo, exp_repo)  # TODO: передать три репозитория
     window.show()
     app.exec_()
