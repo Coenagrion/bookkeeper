913307db379f30c5dade28f80cd3a33c0cb25483
kurtasov <kurtasov@users.noreply.github.com>
2023-03-10 15:12:17 +0300

    A demo for setStretchLastSection(True) in QTableView

---
+++ bookkeeper/gui_demo_03.py
@@ -0,0 +1,51 @@
+"""
+Демонстрация TableView на основе https://www.pythonguis.com/tutorials/qtableview-modelviews-numpy-pandas/
+"""
+
+import sys
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import Qt
+
+
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self._data[0])
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.table = QtWidgets.QTableView()
+
+        data = [(1, 1, 2), (1, 1, 2), (1, 1, 2), ]
+
+        self.model = TableModel(data)
+        self.table.setModel(self.model)
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.setCentralWidget(self.table)
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+window.setFixedSize(580, 600)
+app.exec_()
+
